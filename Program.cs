1a72c0ddfbb8c740035429e3a43c88938c4e85f4
belboy91 <124733232+belboy91@users.noreply.github.com>
2023-03-15 23:46:40 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,40 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Transactions;
+using System.Xml.Linq;
 
-namespace Student_list_HW1
+namespace Survey_HM2
 {
     internal class Program
     {
         static void Main(string[] args)
-        {   
-            
-            Console.WriteLine("How many students?");
-            var userinput = Console.ReadLine();
-            string[] name = new string[20];
-            string[] note = new string[2];
-            for (int i = 0; i < Int32.Parse(userinput); i++)
-                //stringet atalakitjuk int-be
-            {
-                var name[i] = ReadFromConsole("Student's name:");
-                var note[i] = ReadFromConsole("Student's note:");
-
-            }
-            for (int i = 1; i < Int32.Parse(userinput); i++)
-            {
-                Console.WriteLine(name[i], note[i]);
-            }
-        }
-
-        static string ReadFromConsole(string question)
         {
-            var text = "";
-            while (text == "")
-            {
-                Console.WriteLine(question);
-                text = Console.ReadLine();
-            }
-            return text;
-        }
-        
+            List<SurveyClass> Squestions = new List<SurveyClass>();
 
+            //Survey question1 = new Survey();
+            //Survey question2 = new Survey();
+            //Survey question3 = new Survey();
+
+            //Console.WriteLine(question1.GetHashCode());
+            //Console.WriteLine(question2.GetHashCode());
+            //Console.WriteLine(question3.GetHashCode());
+
+            string nextq = "y";
+
+            while (nextq == "y")
+            {
+                SurveyClass question = new SurveyClass();
+
+                Console.WriteLine("Name:");
+                question.Name = Console.ReadLine();
+
+                Console.WriteLine("CNP:");
+                question.CNP = Console.ReadLine();
+
+                Console.WriteLine("Age:");
+                question.Age = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Gender:");
+                question.Gender = Console.ReadLine();
+
+                Console.WriteLine("Birth Month:");
+                question.Month = Console.ReadLine();
+
+                Squestions.Add(question);
+
+                Console.WriteLine("Do you have another client? y/n");
+                nextq = Console.ReadLine();
+
+            }
+            foreach (var item in Squestions)
+            {
+                Console.WriteLine($"Name: {item.Name}, CNP:{item.CNP}, Age:{item.Age}, Gender:{item.Gender}, Borth Month:{item.Month}");
+            }
+
+        }
     }
 }
---
+++ SurveyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Survey_HM2
+{
+    internal class Survey
+    {
+        public string Name { get; set; }
+        public string CNP { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Month { get; set; }
+
+    }
+}
